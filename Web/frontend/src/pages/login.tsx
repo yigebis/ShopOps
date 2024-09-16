c6c1071d5461751117c7abe026b72666b7a4ca24
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-09-16 21:59:23 +0300

    add validation for the forms

--- Web/frontend/src/pages/login.tsx
+++ Web/frontend/src/pages/login.tsx
@@ -1,49 +1,121 @@
-import person from '../assets/person.png'
+import { useState } from "react";
+import person from "../assets/person.png";
+import { Link } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import {useForm , SubmitHandler} from 'react-hook-form'
+import {z} from 'zod';
+import {zodResolver} from "@hookform/resolvers/zod";
+
+
 const Login = () => {
-  return (
-    <div className=" w-screen h-screen  flex  gap-6"> 
-        <div className=" w-[40%] h-full m-11">
-            <img src={person} />
+  const schema = z.object({
+    email:z.string().email(),
+    phoneNumber: z.string().regex( /(\+\s*2\s*5\s*1\s*9\s*(([0-9]\s*){8}\s*))|(0\s*9\s*(([0-9]\s*){8}))/ , "Invalid phone number format"),
+    password:z.string().min(8).regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character'),
+  })
 
-        </div>
-        <div className=" w-[60%] h-full flex py-1">
-            <div className="w-[60%] h-[80%] shadow-custom-shadow p-2 my-10  flex flex-col ">
-                <div className="flex flex-col items-center">
-                <h1 className="text-[25pt]  font-bold" > Shop<span className="text-[#00C8AC]">Ops</span></h1>
-                <p className="text-[#234b48] text-[11pt] mt-3 "> Streamline Your Shop Operations</p>
-                <h1 className="text-[18pt] text-[#5ac1ba] font-bold mt-2">Login</h1>
-                </div>
+  type fields = z.infer<typeof schema>
 
-            <div className="px-7 mt-9">
-                <form>
-               
-                  
-                    <div className="flex flex-col mb-1">
-                    <label className="text-[10pt] text-gray-600"> Email</label>
-                    <input type='email' className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[10pt]" />
-                    </div>
-               
-                    <div className="flex flex-col mb-2">
-                    <label className="text-[10pt] text-gray-600"> Password</label>
-                    <input type="text" className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[10pt]" />
-                    </div>
-                    <div className="flex justify-start ">
-                    <p className="text-[11pt]"> Already have an account? <span className='text-[#5ac1ba] font-bold'> Login Here</span> </p>
-                    </div>
-                     <div className="flex justify-center mt-9 ">
-                     <button className="bg-[#009C86] pb-1 px-8 rounded-[10px] text-white font-bold text-[13pt] text-center">Login</button>
-                     </div>
-                    
-                  
-                    
-                </form>
-            </div>
-            </div>
-
-
-        </div>
-    </div>
+  const { register , handleSubmit , formState:{errors}, clearErrors} = useForm<fields>(
+    {resolver: zodResolver(schema)}
   )
-}
 
-export default Login
+  const onSubmit: SubmitHandler<fields> = (data) =>{
+    console.log(data)
+  }
+
+  const [visible , setVisibility] = useState(false)
+  const handlePasswordVisibility = () => {
+    setVisibility(!visible)
+  }
+
+  return (
+    <div className=" w-screen h-screen  flex  gap-6">
+      <div className=" w-[40%] h-full m-11">
+        <img src={person} />
+      </div>
+      <div className=" w-[60%] h-full flex py-1">
+        <div className="w-[60%] h-[80%] shadow-custom-shadow p-2 my-10  flex flex-col ">
+          <div className="flex flex-col items-center">
+            <h1 className="text-[25pt]  font-bold">
+              {" "}
+              Shop<span className="text-[#00C8AC]">Ops</span>
+            </h1>
+            <p className="text-[#234b48] text-[11pt] mt-3 ">
+              {" "}
+              Streamline Your Shop Operations
+            </p>
+            <h1 className="text-[18pt] text-[#5ac1ba] font-bold mt-2">Login</h1>
+          </div>
+
+          <div className="px-7 mt-3">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col mb-1">
+                <label className="text-[10pt] text-gray-600"> Email</label>
+                <input
+                {...register("email")}
+                type="email"
+                // onChange={() => clearErrors('email')}
+                  className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[10pt]"
+                />
+                {errors.email &&<p className="text-red-600 text-[10pt]"> {errors.email.message}</p> }
+              </div>
+
+              <div className="flex flex-col mb-1">
+                <label className="text-[10pt] text-gray-600">
+                  {" "}
+                  Phone Number
+                </label>
+                <input
+                  type="tel"
+                   {...register("phoneNumber")}
+                  className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[10pt]"
+                />
+                 {errors.phoneNumber &&<p className="text-red-600 text-[10pt]"> {errors.phoneNumber.message}</p> }
+              </div>
+              <div className="flex flex-col mb-1">
+              <label className="text-[10pt] text-gray-600"> Password</label>
+
+              <div className="relative">
+                    <input
+                      type= {visible ? "text" : "password"} 
+                      {...register("password" )}
+                      
+                      className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none border-none px-3 text-[11pt] w-full pr-10 text-base border border-gray-300 "
+                      
+                    />
+                    <div 
+                      className="absolute right-3 top-2 cursor-pointer" onClick={handlePasswordVisibility}
+
+                    >
+                      {visible ? <FaEyeSlash /> : <FaEye />}
+                      
+                    </div>
+                  </div>
+                  {errors.password &&<p className="text-red-600 text-[10pt]"> {errors.password.message}</p> }
+                  </div>
+              <div className="flex justify-start ">
+                <p className="text-[10pt] mt-2 mb-5">
+                  {" "}
+                  Don't have an account ?{" "}
+                  <Link to="/register" className="text-[#5ac1ba] font-bold">
+                    {" "}
+                    Sign Up Here
+                  </Link>{" "}
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <button className="bg-[#009C86] pb-1 px-8 rounded-[10px] text-white font-bold text-[13pt] text-center mb-2" >
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- Web/frontend/src/pages/register.tsx
+++ Web/frontend/src/pages/register.tsx
@@ -1,11 +1,43 @@
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa"; 
+import {Link} from 'react-router-dom'
+import {useForm , SubmitHandler} from 'react-hook-form'
+import {z} from 'zod';
+import {zodResolver} from "@hookform/resolvers/zod";
+
+
 const Register = () => {
+  const schema = z.object({
+    firstName: z.string().min(1 , "first name is required"),
+    lastName:z.string().min(1 , "last name is required"),
+    email:z.string().email(),
+    gender: z.enum(["Male", "Female"], {
+      invalid_type_error: "Select a valid gender", 
+    }),
+    phoneNumber: z.string().regex( /(\+\s*2\s*5\s*1\s*9\s*(([0-9]\s*){8}\s*))|(0\s*9\s*(([0-9]\s*){8}))/ , "Invalid phone number format"),
+    password:z.string().min(8).regex(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, 'Password must contain at least one special character'),
+    confirmPassword: z.string(),
+
+  }).refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Passwords don't match",
+  });
+
+ type fields = z.infer<typeof schema>
+ const { register , handleSubmit , formState:{errors}, clearErrors} = useForm<fields>(
+  {resolver: zodResolver(schema)}
+)
   const [visible , setVisibility] = useState(false)
-  
   const handlePasswordVisibility = () => {
     setVisibility(!visible)
   }
+  const onSubmit: SubmitHandler<fields> = (data) =>{
+    console.log(data)
+  }
+
+ 
+
   return (
     <div className=" h-screen w-screen flex justify-center items-center">
 
@@ -16,9 +48,9 @@ const Register = () => {
             <p className="text-[#234b48] text-[16pt] font-semibold mt-6"> Streamline Your Shop Operations</p>
             <div className="mt-9">
             <p className="text-[12pt] mb-3 text-[#234b48]"> Already have an account?</p>
-            <button className="bg-white py-1 px-5 rounded-[10px] text-[#234b48] font-bold text-[12pt]">
+            <Link to = "/login" className="bg-white py-1 px-5 rounded-[10px] text-[#234b48] font-bold text-[12pt]">
                     {" "} Login
-                  </button>
+                  </Link>
 
             </div>
 
@@ -33,41 +65,53 @@ const Register = () => {
             </div>
 
             <div className="px-10">
-              <form className="flex flex-col gap-1">
+              <form className="flex flex-col gap-1" onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col ">
-                  <label className="text-[10pt] text-gray-600">
-                    {" "}
-                    First Name
-                  </label>
+                <div className="flex justify-between">
+                  <label className="text-[10pt] text-gray-600">First Name</label>
+                  {errors.firstName && <p className="text-[10pt] text-red-600">{errors.firstName.message}</p>}
+                  </div>
                   <input
+                  {...register('firstName')}
                     type="text"
                     className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt]"
                     
                   />
+           
                 </div>
                 <div className="flex flex-col mb-1">
-                  <label className="text-[10pt] text-gray-600">
-                    {" "}
-                    Last Name
-                  </label>
+                <div className="flex justify-between">
+                  <label className="text-[10pt] text-gray-600">Last Name</label>
+                  {errors.lastName && <p className="text-[10pt] text-red-600">{errors.lastName.message}</p>}
+                  </div>
                   <input
+                  {...register('lastName')}
                     type="text"
                     className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt]"
                   />
-                        
+ 
 
                 </div>
                 <div className="flex flex-col mb-1">
-                  <label className="text-[10pt] text-gray-600"> Email</label>
-                  <input
+                <div className="flex justify-between">
+                  <label className="text-[10pt] text-gray-600">First Name</label>
+                  {errors.email && <p className="text-[10pt] text-red-600">{errors.email.message}</p>}
+                  </div>                  <input
+                  {...register('email')}
                     type="email"
                     className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt]"
                   />
+
                 </div>
                 <div className="flex flex-col mb-1">
+                  <div className="flex justify-between">
                   <label className="text-[10pt] text-gray-600">Gender</label>
-                  <select className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt]">
-                  <option value="" selected  hidden> </option>
+                  {errors.gender && <p className="text-[10pt] text-red-600">select one</p>}
+                  </div>
+                
+                  <select className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt]"
+                  {...register('gender')}>
+                  <option value=""   hidden> </option>
                     <option>
                       Female
                     </option>
@@ -75,25 +119,32 @@ const Register = () => {
                       Male
                     </option>
                   </select>
+                  
+
               
                 </div>
                 <div className="flex flex-col mb-1">
-                  <label className="text-[10pt] text-gray-600">
-                    Phone Number
-                  </label>
+                <div className="flex justify-between">
+                  <label className="text-[10pt] text-gray-600"> Phone Number</label>
+                  {errors.phoneNumber && <p className="text-[10pt] text-red-600">{errors.phoneNumber.message}</p>}
+                  </div>
                   <input
                     type="tel"
+                    {...register('phoneNumber')}
                     className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt]"
                   />
-                  
+
                 </div>
                 <div className="flex flex-col mb-1">
-                  <label className="text-[10pt] text-gray-600"> Password</label>
-                  <div className="relative">
+                <div className="flex justify-between gap-5">
+                  <label className="text-[10pt] text-gray-600">Password</label>
+                  {errors.password && <p className="text-[10pt] text-red-600">{errors.password.message}</p>}
+                  </div>                  <div className="relative">
                     <input
+                    {...register('password')}
                       type= {visible ? "text" : "password"} 
                       
-                      className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt] w-full pr-10 text-base border border-gray-300 "
+                      className="h-8 bg-[#F1F5F5] border-none rounded-[5px] outline-none px-3 text-[11pt] w-full pr-10 text-base border border-gray-300 "
                       
                     />
                     <div 
@@ -103,22 +154,25 @@ const Register = () => {
                       {visible ? <FaEyeSlash /> : <FaEye />}
                       
                     </div>
+
                   </div>
            
                 </div>
                 <div className="flex flex-col mb-1">
-                  <label className="text-[10pt] text-gray-600">
-                    {" "}
-                    Confirm Password
-                  </label>
+                <div className="flex justify-between">
+                  <label className="text-[10pt] text-gray-600">Confirm Password</label>
+                  {errors.confirmPassword && <p className="text-[10pt] text-red-600">{errors.confirmPassword.message}</p>}
+                  </div>
                   <input
-                    type="text"
+                  {...register('confirmPassword')}
+                    type="password"
                     className="h-8 bg-[#F1F5F5] rounded-[5px] outline-none px-3 text-[11pt]"
                   />
+
                 </div>
 
                 <div className="flex justify-center  ">
-                  <button className="bg-[#009C86]  py-1.5 px-8 rounded-[10px] text-white font-bold text-[12pt]">
+                  <button type="submit" className="bg-[#009C86]  py-1.5 px-8 rounded-[10px] text-white font-bold text-[12pt]">
                     {" "}
                     Sign Up
                   </button>

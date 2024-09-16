1e9a10fb8cc99b29163118e3f826d97ad6d3c776
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-09-16 21:57:33 +0300

    add routes for the pages

--- Web/frontend/src/App.tsx
+++ Web/frontend/src/App.tsx
@@ -1,12 +1,20 @@
-
 import './App.css'
 import Register from './pages/register'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login';
+
 
 function App() {
 
   return (
     <>
-    <Register />
+    <Router>
+      <Routes>
+        <Route path = "/login" element = {<Login />}/>
+        <Route path = "/register" element = {<Register />}/>
+      </Routes>
+    </Router>
+    
 
 
     </>
